ca91e3967346d7ba83485f23b732fc27df5ff465
nadavshaham <37561219+nadavshaham@users.noreply.github.com>
2022-10-31 10:24:55 +0200

    added multi word support

--- translate.js
+++ translate.js
@@ -1,16 +1,42 @@
 const got = require("got");
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
-const wordToTranslate = process.argv[2];
-const vgmUrl = "https://www.morfix.co.il/en/" + wordToTranslate;
+const english = /^[A-Za-z0-9]*$/;
+const arr = [];
+const firstChar = process.argv[2];
+let vgmUrl = "https://www.morfix.co.il/en/";
+for (let i = 2; i < process.argv.length; i++) {
+  arr.push(process.argv[i]);
+  vgmUrl += process.argv[i];
+  if (i != process.argv.length - 1) {
+    vgmUrl += "%20";
+  }
+}
 got(vgmUrl)
   .then((response) => {
-    console.log(
-      new JSDOM(response.body).window.document
-        .getElementsByClassName("normal_translation_div")
-        .item(0)
-        .innerHTML.replace(/<span.*<\/span>/, " ")
-    );
+    let raw;
+
+    if (process.argv.length >= 4) {
+      // if more than one word different class is needed
+      if (english.test(firstChar)) {
+        // if english different class is needed
+        raw = new JSDOM(response.body).window.document.getElementsByClassName(
+          "MachineTranslation_divfootertop_enTohe"
+        );
+      } else {
+        raw = new JSDOM(response.body).window.document.getElementsByClassName(
+          "MachineTranslation_divfootertop_heToen"
+        );
+      }
+    } else if (process.argv.length <= 3) {
+      raw = new JSDOM(response.body).window.document.getElementsByClassName(
+        "normal_translation_div"
+      );
+    } else {
+      error("too little args");
+    }
+    const trimmed = raw.item(0).innerHTML.replace(/<span.*<\/span>/, " ");
+    console.log(trimmed.trim());
   })
   .catch((err) => {
     if (
